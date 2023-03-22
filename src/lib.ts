13dfb94c67ff1904d76589bd20e2a84210f1f3fc
Olegdrn <105755089+Olegdrn@users.noreply.github.com>
2023-03-23 00:05:47 +0300

    Add files via upload

---
+++ src/lib.ts
@@ -0,0 +1,32 @@
+export function renderBlock(elementId, html) {
+  const element = document.getElementById(elementId)
+  element.innerHTML = html
+}
+
+export function renderToast(message, action) {
+  let messageText = ''
+
+  if (message != null) {
+    messageText = `
+      <div id="info-block" class="info-block ${message.type}">
+        <p>${message.text}</p>
+        <button id="toast-main-action">${action?.name || 'Закрыть'}</button>
+      </div>
+    `
+  }
+
+  renderBlock(
+    'toast-block',
+    messageText
+  )
+
+  const button = document.getElementById('toast-main-action')
+  if (button != null) {
+    button.onclick = function () {
+      if (action != null && action.handler != null) {
+        action.handler()
+      }
+      renderToast(null)
+    }
+  }
+}
